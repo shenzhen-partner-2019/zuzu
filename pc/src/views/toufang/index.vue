39bfee0a6be9bb1c6fdfbca60c965d167ec1c9d6
bluprint1453 <luozl2015@foxmail.com>
2019-11-06 22:42:57 +0800

    投放房源页面基本搭建完毕

--- pc/src/views/toufang/index.vue
+++ pc/src/views/toufang/index.vue
@@ -22,8 +22,12 @@
             <span class="radio-wrapper">
               <input type="radio" name="identity">
               <label @click="selectIdentity(2)"><span :class="{circle: identity === 2}"></span></label>
-              <span>我是转租</span>
-              <i class="icon-tip"></i>
+              <span class="radio-text">我是转租</span>
+              <span class="icon-tip" @click.stop="toggleTipVisible"></span>
+              <div class="tip-content" v-show="tipVisible">
+                <div class="tip-text">在转租前，请先和业主沟通，确认其同意转租。</div>
+                <span></span>
+              </div>
             </span>
           </div>
         </div>
@@ -34,18 +38,31 @@
         </div>
         <div class="form-item form-item-bg form-item-blur">
           <div class="input-wrapper">
-            <div class="input-box"><input type="text" placeholder="详细地址"></div>
+            <select class="select-district" name="district" id="district">
+              <option value="0">区域</option>
+              <option value="1">南山</option>
+            </select>
+            <span class="split-line"></span>
+            <div class="input-box input-box-district"><input type="text" placeholder="详细地址"></div>
           </div>
         </div>
          <div class="form-item form-item-bg form-item-blur">
           <div class="input-wrapper">
             <div class="input-box"><input type="text" placeholder="出租面积"></div>
+            <span class="rent-unit">m²</span>
           </div>
         </div>
          <div class="form-item form-item-bg form-item-blur">
           <div class="input-wrapper">
-            <div class="input-box"><input type="text" placeholder="租金"></div>
+            <div class="input-box input-box-rent"><input type="text" placeholder="租金"></div>
+            <span class="split-line"></span>
+           <select class="select-rent" name="rent" id="rent">
+              <option value="1">元/工位/月</option>
+              <option value="2">元/m²/月</option>
+              <option value="3">元/月</option>
+            </select>
           </div>
+          
         </div>
         <div class="pay-info">
           <p class="pay-title">佣金</p>
@@ -64,7 +81,8 @@
         </div>
         <div class="form-item form-item-bg form-item-blur">
           <div class="input-wrapper">
-            <div class="input-box"><input type="text" placeholder="验证码"></div>
+            <div class="input-box-code"><input type="text" placeholder="验证码"></div>
+            <span class="btn-code">获取短信验证码</span>
           </div>
         </div>
         <div class="form-item form-item-submit">
@@ -80,17 +98,26 @@ export default {
   data() {
     return {
       identity: -1,
+      tipVisible: false,
       checkedPayType: -1,
       payTypelist: ["无", "0.5个月", "1个月", "1.5个月", "2个月"]
     };
+  },
+  mounted() {
+    window.addEventListener('click', () => {
+      this.tipVisible = false
+    })
   },
   methods: {
     selectPayType(i) {
       this.checkedPayType = i;
     },
     selectIdentity(i) {
-      console.log('se', i)
-      this.identity = i
+      console.log("se", i);
+      this.identity = i;
+    },
+    toggleTipVisible() {
+      this.tipVisible = !this.tipVisible
     }
   }
 };
@@ -102,6 +129,7 @@ export default {
 
 .toufang {
   position: relative;
+  height: 1138px;
   .bg-toufang {
     height: 260px;
     background: url("../../../public/img/bg_toufang.jpg") no-repeat;
@@ -137,7 +165,7 @@ export default {
       margin-bottom: 30px;
     }
     .flag-code {
-      margin-bottom: 20px;
+      margin-bottom: 16px;
     }
     .user-title {
       height: 30px;
@@ -178,6 +206,7 @@ export default {
 
     .radio-wrapper {
       margin-right: 20px;
+      position: relative;
       [type="radio"] {
         display: none;
       }
@@ -206,6 +235,43 @@ export default {
         display: inline-block;
         vertical-align: middle;
       }
+      .icon-tip {
+        display: inline-block;
+        height: 15px;
+        width: 15px;
+        background: url("../../../public/img/wenhao.png") no-repeat;
+        background-size: 100%;
+        vertical-align: middle;
+        margin-left: 4px;
+        cursor: pointer;
+      }
+      .tip-content {
+        position: absolute;
+        top: -14px;
+        right: -286px;
+        width: 276px;
+        background: #fffbe3;
+        border: solid 1px #ece7c8;
+        height: 36px;
+        line-height: 36px;
+        font-size: 12px;
+        padding: 0 10px;
+        box-shadow: 0 0 0 rgba(0, 0, 0, 0.5);
+        margin-top: 3px;
+        border-radius: 4px;
+        span {
+          position: absolute;
+          display: block;
+          width: 10px;
+          height: 12px;
+          background: url('../../../public/img/tip_arrow.png') no-repeat 0 0;
+          font-size: 0;
+          background-size: 60%;
+          left: -6px;
+          top: 50%;
+          margin-top: -6px;
+        }
+      }
     }
     .form-item {
       height: 48px;
@@ -218,15 +284,77 @@ export default {
       &:focus-within {
         border: 1px solid $form-focus-color;
       }
-      
 
       .input-wrapper {
         display: block;
+        position: relative;
+      }
+      .rent-unit {
+        position: absolute;
+        right: 0px;
+        top: 0;
+        display: block;
+        height: 14px;
+        line-height: 14px;
+        top: 16px;
+        color: #aaa;
       }
       .input-box {
-        display: inline-block;
         width: 100%;
+        &.input-box-district {
+          display: inline-block;
+          width: 72%;
+          padding-left: 20px;
+        }
+        &.input-box-rent {
+          display: inline-block;
+          width: 72%;
+        }
       }
+      .select-district {
+        width: 28%;
+        display: inline-block;
+        height: 48px;
+        border: none;
+        background: none;
+        text-align: center;
+        color: #666;
+        &:focus {
+          outline: none;
+        }
+        &:optional {
+          margin-left: -20px;
+          margin-right: 10px;
+          line-height: 24px;
+          padding-left: 20px;
+        }
+      }
+      .select-rent {
+        width: 28%;
+        display: inline-block;
+        height: 48px;
+        border: none;
+        background: none;
+        text-align: center;
+        color: #666;
+        &:focus {
+          outline: none;
+        }
+        &:optional {
+          margin-right: -20px;
+          line-height: 24px;
+          padding-left: 20px;
+        }
+      }
+      .split-line {
+        display: inline-block;
+        width: 1px;
+        height: 14px;
+        background: #bbb;
+        margin-top: 4px;
+        transform: translateY(2px);
+      }
+
       input {
         background: #f5f5f5;
         width: 100%;
@@ -239,6 +367,20 @@ export default {
     }
     .form-item-bg {
       background-color: #f5f5f5;
+    }
+    .input-box-code {
+      padding-right: 100px;
+    }
+    .btn-code {
+      position: absolute;
+      display: inline-block;
+      right: 0;
+      font-size: 12px;
+      color: #aaa;
+      height: 18px;
+      line-height: 18px;
+      top: 16px;
+      cursor: pointer;
     }
     .form-item-submit {
       text-align: center;
